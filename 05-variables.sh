da3ca68ef6f699ae14b84f15b5ebc5594cdc0a17
Nagaraju kumar <rajkumarkolusu222@gmail.com>
2025-07-09 13:00:03 +0530

    user name and password

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please Enter Your UserName::"
+
+read USERNAME # USERNAME is variable 
+
+echo "Please Enter Password::"
+
+read PASSWORD # PASSWORD IS variable
+
+echo "UserName is $USERNAME, Pasaword is $PASSWORD"
