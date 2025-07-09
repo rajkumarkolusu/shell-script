9f8b10c7ad464ccfb9280d4cc16a710e2ade7c18
Nagaraju kumar <rajkumarkolusu222@gmail.com>
2025-07-09 12:14:22 +0530

    hellow script

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh # no space here b/w = and value 
+PERSON2=Suresh # no space here  b/w = and value 
+
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. How is your work?"
+echo "$PERSON2:: Not bad. I am thinking to upgrade to DevOps"
