9a3c71173c587cdc8637615eba0410d1e867bc81
Nagaraju kumar <rajkumarkolusu222@gmail.com>
2025-07-09 14:02:58 +0530

    hellow script

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,4 +3,5 @@
 echo "All Variables: $@"
 echo "Number of variables Passed: $#"
 echo "TO Know Script Name: $0"
-echo "Current Working Diractory: $PWD"
+echo "Current Working Directory: $PWD"
+echo "Home Directory of current user:$HOME"
