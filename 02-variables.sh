62fcc8af3cc5c1701734f81b843ce552aefa7fb3
Nagaraju kumar <rajkumarkolusu222@gmail.com>
2025-07-09 12:06:38 +0530

    hellow script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-This is again sample script
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: I am fine too. How is your work?"
+echo "Suresh:: Not bad. I am thinking to upgrade to DevOps"
