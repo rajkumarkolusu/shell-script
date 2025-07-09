be63d263f9d4ccbe15fff580d3c3aaca9c6871da
Nagaraju kumar <rajkumarkolusu222@gmail.com>
2025-07-09 14:27:31 +0530

    hellow script

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 10]
+if [ $NUMBER -gt 10 ]
 then 
     echo "Given Number $NUMBER is greater than 10"
 else 
-    echo "Given Number @NUMBER is Less than 10"
+    echo "Given Number $NUMBER is Less than 10"
 fi
