f3725b269db5972c1eac62b63377468c1fa79330
Nagaraju kumar <rajkumarkolusu222@gmail.com>
2025-07-24 11:13:16 +0530

    shell

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,6 @@ COURSE="DevOps from current script"
 echo "Before calling other script, course: $COURSE"
 echo "process ID of current shell script: $$"
 
-#./16-others-scripts.sh 
+./16-others-scripts.sh 
 
 echo "After calling other script, course: $COURSE"
