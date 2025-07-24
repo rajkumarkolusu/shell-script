ad40c19af99f52d9e7e9f5b1f1f00c1ae0273104
Nagaraju kumar <rajkumarkolusu222@gmail.com>
2025-07-24 10:44:23 +0530

    expense-shell

---
+++ 15-current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE="DevOps from current script"
+
+echo "Before calling other script, course: $COURSE"
+echo "process ID of current shell script: $$"
+
+#./16-others-scripts.sh 
+
+echo "After calling other script, course: $COURSE"
